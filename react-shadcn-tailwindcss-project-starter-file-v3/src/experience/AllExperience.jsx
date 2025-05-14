8d44ce77a0c8ec3d6c942bb2a65794ba3f551d52
Aditya Mittal <148564962+AdityaMittal333@users.noreply.github.com>
2025-05-14 21:24:36 +0530

    Update AllExperience.jsx

--- react-shadcn-tailwindcss-project-starter-file-v3/src/experience/AllExperience.jsx
+++ react-shadcn-tailwindcss-project-starter-file-v3/src/experience/AllExperience.jsx
@@ -17,7 +17,7 @@ export default function AllExperience() {
   useEffect(() => {
     const fetchProjects = async () => {
       try {
-        const res = await fetch("http://localhost:3000/api/experience",{
+        const res = await fetch(`${import.meta.env.VITE_API_URL}/experience`,{
           method:"GET",
           headers:{
             "Content-Type":"application/json",
