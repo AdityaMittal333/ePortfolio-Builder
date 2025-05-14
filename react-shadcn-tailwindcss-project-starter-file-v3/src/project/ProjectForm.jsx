3aa045d6dd9937f77fea149005c2be791e2cea17
Aditya Mittal <148564962+AdityaMittal333@users.noreply.github.com>
2025-05-14 21:30:30 +0530

    Update ProjectForm.jsx

--- react-shadcn-tailwindcss-project-starter-file-v3/src/project/ProjectForm.jsx
+++ react-shadcn-tailwindcss-project-starter-file-v3/src/project/ProjectForm.jsx
@@ -48,7 +48,7 @@ export default function ProjectForm() {
       formData.append("image", image);
     }
 
-    const res = await fetch("http://localhost:3000/api/projects/create", {
+    const res = await fetch(`${import.meta.env.VITE_API_URL}/projects/create`, {
       method: "POST",
       body: formData,
     });
