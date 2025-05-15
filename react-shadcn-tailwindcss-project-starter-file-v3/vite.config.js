61d5bc030aaad8c7dc03ec95dd6c4b71c3ee6052
Aditya Mittal <148564962+AdityaMittal333@users.noreply.github.com>
2025-05-15 10:21:43 +0530

    Update vite.config.js

--- react-shadcn-tailwindcss-project-starter-file-v3/vite.config.js
+++ react-shadcn-tailwindcss-project-starter-file-v3/vite.config.js
@@ -1,14 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from "path"
+import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      src: path.resolve(__dirname, "./src"),
-      "@": path.resolve(__dirname, "./src"),
+      src: path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, './src'),
     },
+  },
+  server: {
+    // Enables fallback for client-side routing in dev
+    historyApiFallback: true,
   },
 })
